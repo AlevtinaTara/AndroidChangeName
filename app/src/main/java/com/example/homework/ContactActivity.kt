b4290b8e7d14e59d652552489394d48f2adee66f
AlevtinaTara <alevtina023@gmail.com>
2024-03-31 23:46:03 +0200

    Add switch to contact

--- app/src/main/java/com/example/homework/ContactActivity.kt
+++ app/src/main/java/com/example/homework/ContactActivity.kt
@@ -1,15 +1,35 @@
 package com.example.homework
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.materialswitch.MaterialSwitch
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
+        val favoriteButton = findViewById<MaterialButton>(R.id.contact_favorite)
+        val imageSwitch = findViewById<MaterialSwitch>(R.id.contact_photo_switch)
+        val avatar = findViewById<ImageView>(R.id.contact_avatar)
+
+        favoriteButton.setOnClickListener { v ->
+            favoriteButton.isSelected = !favoriteButton.isSelected
+        }
+
+        imageSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                avatar.visibility = View.GONE
+            } else {
+                avatar.visibility = View.VISIBLE
+            }
+        }
     }
+
 }
